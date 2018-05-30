31f31c992ae62073a66f8383a795d5f6a6dc374f
Ahmad <ahmad.k.farag@gmail.com>
2018-05-30 18:55:09 -0400

    Added Export

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,4 @@ export * from "./orchestration/strategy/DispatchStrategy";
 export * from "./store/JobResultStore";
 export * from "./store/JobStore";
 export * from "./util/queue/Queue";
+export * from "./client/ConductorClient"
